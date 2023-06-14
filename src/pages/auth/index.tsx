0a88939b080a5e677a7c9140415b0739932598d6
Edita Dani <editasefullaj@gmail.com>
2023-06-14 17:07:20 +0200

    add hero image

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/componenets/Navbar/Navbar";
 import React from "react";
+import Image from "next/image";
+import AuthModal from "@/componenets/Navbar/Modals/AuthModal";
 
 type AuthPageProps = {};
 
@@ -8,6 +10,10 @@ const AuthPage: React.FC<AuthPageProps> = () => {
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
         <Navbar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+          <Image src="/hero.png" alt="Hero img" width={700} height={700} />
+        </div>
+        <AuthModal />
       </div>
     </div>
   );
