c062afdd43eb7627965da2ad4359a48a870be630
Edita Dani <editasefullaj@gmail.com>
2023-07-04 02:03:58 +0200

    add close modal functionality

--- src/componenets/Navbar/Modals/AuthModal.tsx
+++ src/componenets/Navbar/Modals/AuthModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IoClose } from "react-icons/io5";
 import Login from "./Login";
 import Signup from "./Signup";
@@ -13,7 +13,10 @@ const AuthModal: React.FC<AuthModalProps> = () => {
   const closeModal = useCloseModal();
   return (
     <>
-      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60"></div>
+      <div
+        className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60"
+        onClick={closeModal}
+      ></div>
       <div className="w-full sm:w-[450px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center">
         <div className="relative w-full h-full mx-auto flex items-center justify-center">
           <div className="bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-brand-orange to-slate-900 mx-6">
@@ -51,5 +54,13 @@ function useCloseModal() {
       type: "login",
     }));
   };
+
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") closeModal();
+    };
+    window.addEventListener("keydown", handleEsc);
+    return window.removeEventListener("keydown", handleEsc);
+  }, []);
   return closeModal;
-};
+}
